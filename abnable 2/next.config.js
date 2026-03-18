573b380beac8383d513e4b6fa7be85f211637f64
aarongit412 <yannickdarma@gmail.com>
2026-03-18 20:31:09 +0000

    Delete abnable 2 directory

--- abnable 2/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Serve our existing HTML files as static assets from /public
-  // API routes handle all backend logic
-}
-module.exports = nextConfig
